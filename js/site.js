247f6550441b4bf8ab9d00358f479c6f74cd15b7
Phil Daum <philgdaum@gmail.com>
2022-04-05 13:33:02 -0700

    add chatbot pseudocode

---
+++ js/site.js
@@ -0,0 +1,9 @@
+function adminChatBot() {
+
+    //take string text from user entry
+    //print it above
+    //ping same text to chatbot entry
+    //copy chatbot responce
+    //print chatbot responce in monotype
+
+}

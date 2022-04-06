aa6687df83fe748f2b236df906403d46e42a7f51
Phil Daum <philgdaum@gmail.com>
2022-04-05 19:34:25 -0700

    i can learn how to merge for this

--- js/site.js
+++ js/site.js
@@ -1,7 +1,15 @@
 //my chatbot function
 
 function adminChatBot() {
+    //take string text from user entry
+    document.getElementById("user input").value;
+    
+     //i think it should know that it is textarea
+    //print it above
 
+    //ping same text to chatbot entry
+    //copy chatbot responce
+    //print chatbot responce in monotype
 }
 
 

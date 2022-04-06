8914beb8ac52d321a075dfcd9fe18db9e81ba8b6
Phil Daum <philgdaum@gmail.com>
2022-04-05 21:50:06 -0700

    basicer functions

--- js/site.js
+++ js/site.js
@@ -2,11 +2,11 @@
 
 function adminChatBot() {
     //take string text from user entry
-    document.getElementById("user input").value;
+   let user = document.getElementById("chat").value;
     
      //i think it should know that it is textarea
     //print it above
-
+    document.getElementById("user input").innerHTML = user;
     //ping same text to chatbot entry
     //copy chatbot responce
     //print chatbot responce in monotype

1d3843088d614e320b8c659fe5a7c431b5d72d57
Phil Daum <philgdaum@gmail.com>
2022-04-05 19:09:10 -0700

    prep to mix branches

--- js/site.js
+++ js/site.js
@@ -2,24 +2,6 @@
 
 function adminChatBot() {
 
-  //from chatbot
-  SDK.applicationId = "5308744934602165052";
-  var sdk = new SDKConnection();
-  var web = new WebChatbotListener();
-  web.connection = sdk;
-  web.instance = '42609462';
-  web.instanceName = 'admin';
-  web.nativeVoice = true;
-  web.nativeVoiceName = "";
-  web.lang = "en";
-  SDK.lang = "en";
-  web.greet();
-  document.getElementById('user input').addEventListener('keypress', function(event) {
-    if (event.keyCode == 13) {
-      web.sendMessage();
-      return false;
-    }
-  });
 }
 
 

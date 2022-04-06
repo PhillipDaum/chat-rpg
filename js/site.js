32131df5c39f6fd7774bb729dd315eea987ac015
Phil Daum <philgdaum@gmail.com>
2022-04-05 18:52:48 -0700

    basic functionality

--- js/site.js
+++ js/site.js
@@ -1,63 +1,25 @@
-//ALLOW enter key to do stuff while user is in textbox
-//https://www.pluralsight.com/guides/how-to-enter-key-event-handler-on-a-react-bootstrap-input-component
-import React, { Component } from "react";
-import { InputGroup, FormControl, Input } from "react-bootstrap";
-
-class SimpleKeyEvent extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: "React-bootstrap key enter event"
-    };
-    this.onKeyUp = this.onKeyUp.bind(this);
-  }
-
-  onKeyUp(event) {
-    if (event.charCode === 13) {
-      this.setState({ inputValue: event.target.value });
-    }
-  }
-
-  render() {
-    const { inputValue } = this.state;
-
-    return (
-      <div>
-        <InputGroup>
-          <FormControl placeholder="First name" onKeyPress={this.onKeyUp} />
-        </InputGroup>
-        <hr />
-        <span>Input value is : {inputValue}</span>
-      </div>
-    );
-  }
-}
-
-export default SimpleKeyEvent;
-
-
-//can write onKeyUp(enter) to make stuff go
-onKeyUp(event) {
-    if (event.key === "enter") {
-      this.setState({ inputValue: event.target.value });
-    }
-}
-
-
 //my chatbot function
 
 function adminChatBot() {
 
-
-    //take string text from user entry
-    document.getElementById("user input");
-     //i think it should know that it is textarea
-    //print it above
-    print(innerText);
-   
-    //ping same text to chatbot entry
-    //copy chatbot responce
-    //print chatbot responce in monotype
-
+  //from chatbot
+  SDK.applicationId = "5308744934602165052";
+  var sdk = new SDKConnection();
+  var web = new WebChatbotListener();
+  web.connection = sdk;
+  web.instance = '42609462';
+  web.instanceName = 'admin';
+  web.nativeVoice = true;
+  web.nativeVoiceName = "";
+  web.lang = "en";
+  SDK.lang = "en";
+  web.greet();
+  document.getElementById('user input').addEventListener('keypress', function(event) {
+    if (event.keyCode == 13) {
+      web.sendMessage();
+      return false;
+    }
+  });
 }
+
 

9a9723039eeb714dadf849c99fa181a201bd7b6e
Phil Daum <philgdaum@gmail.com>
2022-04-12 15:47:44 -0700

    styling

--- js/index.js
+++ js/index.js
@@ -159,11 +159,11 @@ const botReplies = [
         "I don't know actually"
     ],
     //3
-    ["I am infinite"],
+    ["I am infinite", "I'm pretty new, still working out some bugs"],
     //4
-    ["I am just a bot", "I am a bot. What are you?"],
+    ["I am just a bot", "I am a bot. What are you?", "you can think of my like an extension of Phil, but I'm confused a lot"],
     //5
-    ["The one true God, JavaScript"],
+    ["The one true God, JavaScript", "Phil"],
     //goes to my LinkedIn
     ["<p>Would you like to see Phil's LinkedIn? </p> <a class='btn btn-info ms-5' target='_blank' href='https://www.linkedin.com/in/daumphil/'> <i class='fa fs-1 p-2 fa-linkedin'></i></a>"],
     //goes to my GitHub
